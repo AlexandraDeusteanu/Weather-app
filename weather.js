3da28a80dc8c48f2b0749c984c786e1ed6ff29a7
Your Name <dallexandra30@gmail.com>
2022-02-18 23:41:05 +0200

    covered edge case

--- weather.js
+++ weather.js
@@ -4,16 +4,24 @@ const desc = document.querySelector(".description")
 const temperature = document.querySelector(".temperature")
 const humidityy = document.querySelector(".humidity")
 
+
+
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
     const searchedCity = form.elements.cityNameValue.value;
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=metric&appid=ecd630c1c3bad6e553abf4bc0b863fbb`)
+    await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=metric&appid=ecd630c1c3bad6e553abf4bc0b863fbb`)
+    .then(response => {
+            cityName.innerHTML = searchedCity;
+            desc.innerHTML = response.data.weather[0].description.toUpperCase();
+            temperature.innerHTML = `Temperature is ${response.data.main.temp}°C`;
+            humidityy.innerHTML = `Humidity is ${response.data.main.humidity} %`;
+            e.target.reset();
+        })
+            .catch(err => {
+                console.log(err)
+                alert("No city with this name")
+                e.target.reset()});
 
-    cityName.innerHTML = searchedCity
-    desc.innerHTML = response.data.weather[0].description.toUpperCase();
-    temperature.innerHTML = `Temperature is ${response.data.main.temp}°C`
-    humidityy.innerHTML = `Humidity is ${response.data.main.humidity} %`
-    console.log(response)
-    e.target.reset();
+   
 })
 
